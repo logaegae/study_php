60747a8167aedf13d9477236ee3c8e2fb2fa68d3
BY Kim <logaegae@gmail.com>
2016-10-30 12:04:34 +0900

    161030

---
+++ public_html/web/defineRequire.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Document</title>
+    <!-- 합쳐지고 최소화된 최신 CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+    <!-- 부가적인 테마 -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+    <!-- 합쳐지고 최소화된 최신 자바스크립트 -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <div class="container">
+        <?php
+            //어플리케이션 상수를 정의
+            require_once('appvars.php');
+            require_once('connectvars.php');
+            echo '<table>';
+            $i = 0;
+            while ($row = mysql_fetch_array($data)){
+
+            }
+        ?>
+
+    </div>
+</body>
+</html>
---
+++ public_html/web/require/connectvars.php
@@ -0,0 +1,6 @@
+<?php
+    define('DB_HOST','ftp.antagora.esy.es');
+    define('DB_USER','u733252017_logae');
+    define('DB_PASSWORD','a5695938');
+    define('DB_NAME','u733252017_mydb');
+ ?>
